be68d8ac3b98a92489a0ab6355585bad9de2b864
samms206 <sh2069936@gmail.com>
2024-11-29 14:01:39 +0700

    updated

--- api/index.js
+++ api/index.js
@@ -1,21 +1,35 @@
 require('dotenv').config();
-const path = require('path');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const cors = require('cors');
-
-// CORS settings
-const corsOptions = {
-  origin: ["https://waaxx.webflow.io", "http://localhost:3000"],
-  optionsSuccessStatus: 200,
-};
+const cors = require('cors');  // Import CORS
 
 module.exports = async (req, res) => {
-  // Apply CORS
-  cors(corsOptions)(req, res, () => {});
+  // Apply CORS middleware
+  cors({
+    origin: ["https://waaxx.webflow.io", "http://localhost:3000", "https://stripe-payment-gateway-five.vercel.app"], // Sesuaikan dengan domain frontend Anda
+    optionsSuccessStatus: 200,
+  })(req, res, () => {});
+
+  if (req.method === 'POST' && req.url === '/payment') {
+    try {
+      const amount = req.body.amount;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100,  // Convert to cents
+        currency: 'usd',
+        payment_method_types: ['card'],
+        payment_method: req.body.paymentMethodId,
+        confirm: true,
+        error_on_requires_action: true
+      });
+
+      res.json({ message: 'Payment successful' });
+      console.log(paymentIntent);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
 
   if (req.method === 'POST' && req.url === '/save-card') {
     try {
-      // CREATE customer
       const customer = await stripe.customers.create({
         name: 'Gwen Stacy',
         email: 'gwenstacy101@gmail.com',
@@ -26,27 +40,7 @@ module.exports = async (req, res) => {
       });
 
       console.log(customer);
-
       res.status(201).json({ message: 'Card saved successfully' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  }
-
-  if (req.method === 'POST' && req.url === '/payment') {
-    try {
-      const amount = req.body.amount;
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount * 100,
-        currency: 'usd',
-        payment_method_types: ['card'],
-        payment_method: req.body.paymentMethodId,
-        confirm: true,
-        error_on_requires_action: true
-      });
-
-      res.json({ message: 'Payment successful' });
-      console.log(paymentIntent);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
