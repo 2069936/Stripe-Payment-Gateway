9fcc38e6bdcae5bc1277a18621bed6c480a46d0f
samms206 <sh2069936@gmail.com>
2024-11-29 14:17:58 +0700

    updated

--- api/index.js
+++ api/index.js
@@ -2,25 +2,20 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const cors = require('cors');
 const express = require('express');
-
 const app = express();
-
-// Menggunakan CORS sebagai middleware
 const corsOptions = {
-  origin: ["https://waaxx.webflow.io", "http://localhost:3000"], // Ganti dengan domain yang sesuai
+  origin: ["https://stripe-payment-gateway-five.vercel.app"], 
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));  // Apply CORS di sini, untuk semua request
+app.use(cors(corsOptions)); 
+app.use(express.json());
 
-app.use(express.json()); // Memungkinkan parsing JSON body
-
-// API untuk menangani pembayaran
 app.post('/api/payment', async (req, res) => {
   try {
     const { amount, paymentMethodId } = req.body;
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100, // Konversi ke cents
+      amount: amount * 100,
       currency: 'usd',
       payment_method_types: ['card'],
       payment_method: paymentMethodId,
@@ -33,5 +28,4 @@ app.post('/api/payment', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
 module.exports = app;
